2aac58fda019b5b78d27541543fd1c374b5b13d4
Matthew Gerring <gerring.matthew@gmail.com>
2012-08-23 15:11:23 -0700

    debug on Heroku

--- repsf/urls.py
+++ repsf/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,7 +17,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if not DEBUG:
+if not settings.DEBUG:
 	urlpatterns += patterns('',
 		url(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 	)
