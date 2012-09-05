2528a3224f43adebb7f8ba605e5683eebc990902
Matthew Gerring <gerring.matthew@gmail.com>
2012-09-04 17:53:04 -0700

    model admin"

--- repsf/map/admin.py
+++ repsf/map/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from repsf.map.models import Location
 
-admin.site.register(Location)
+admin.site.register(LocationAdmin)
+
+class LocationAdmin(admin.ModelAdmin):
+	list_display = ['name', 'address', 'fix_address']
