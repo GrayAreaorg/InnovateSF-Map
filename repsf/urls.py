0c245c640ce0e4f5570d4dd0fa1261edeca8141e
Matthew Gerring <gerring.matthew@gmail.com>
2012-09-04 17:33:03 -0700

    static files

--- repsf/urls.py
+++ repsf/urls.py
@@ -18,5 +18,5 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
-	url(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
