fb616a07816d1ec730816368b6f2c60f711315b7
Matthew Gerring <gerring.matthew@gmail.com>
2012-09-10 15:36:18 -0700

    reordering urls

--- repsf/urls.py
+++ repsf/urls.py
@@ -9,19 +9,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-	url(r'^(?P<location>\w+)/?$', 'repsf.map.views.home', name='spec_loc'),
-    # url(r'^repsf/', include('repsf.foo.urls')),
-
+	
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'repsf.map.views.home', name='home'),
 	url(r'^accounts/login/?$', 'django.contrib.auth.views.login', {'template_name':'login.html', 'authentication_form': EmailAuthenticationForm}, name='login'),
 	url(r'^accounts/create/?$', 'repsf.accounts.views.create', name='register'),
 	url(r'^accounts/profile/?$', 'repsf.accounts.views.read', name='show_user'),
 	url(r'^accounts/logout/?$', 'repsf.accounts.views.end_session', name='logout'),
-	url(r'^$', 'repsf.map.views.home', name='home'),
+	url(r'^(?P<location>\w+)/?$', 'repsf.map.views.home', name='spec_loc'),
 )
 
 urlpatterns += patterns('',
